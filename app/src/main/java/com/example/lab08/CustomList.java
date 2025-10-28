4b6f2417ffa72510718c1467de7a26bdc9bf25aa
Hualin457 <hualin@ualberta.ca>
2025-10-28 17:55:04 -0600

    implement CustomList::deleteCity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,6 +18,17 @@ public class CustomList {
         return cities.contains(city);
     }
 
+    public void deleteCity(City city) {
+        if (!cities.contains(city)) {
+            throw new IllegalStateException(
+                "City " + city.getCityName() +
+                    " in " + city.getProvinceName() +
+                    " does not exists."
+            );
+        }
+        cities.remove(city);
+    }
+
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
 }
